1b5af901dd4114910133d93185b53ee355edfadb
Oleksandr Smolenkov <oleksandr_smolenkov@epam.com>
2021-11-15 01:53:50 +0200

    add airline

---
+++ HomeWork4/src/Task1/plane/instances/PlaneCessna172.java
@@ -0,0 +1,11 @@
+package Task1.plane.instances;
+
+import Task1.plane.hierarchy.LightPassengerPlane;
+
+public class PlaneCessna172 extends LightPassengerPlane {
+
+	public PlaneCessna172() {
+		super(3, 300, 200, "Cessna", "172");
+	}
+
+}
---
+++ HomeWork4/src/Task1/plane/instances/PlaneDouglasMD11F.java
@@ -0,0 +1,9 @@
+package Task1.plane.instances;
+
+import Task1.plane.hierarchy.CargoPlane;
+
+public class PlaneDouglasMD11F extends CargoPlane {
+	public PlaneDouglasMD11F() {
+		super(42000, 10000, 3400, "McDonnell Douglas", "MD-11F");
+	}
+}
